6eb6a22fd17db9a6bd0a79ea79a977ebfec81920
Bill S <bill.e.seaman@gmail.com>
2018-11-20 22:33:56 -0800

    update repo

---
+++ ImplementApp/ImplementApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ImplementApp
+//
+//  Created by Loud on 11/20/18.
+//  Copyright © 2018 William Seaman. All rights reserved.
+//
+
+import UIKit
+import TestFramework
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    var tmp = testModel.self
+    
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
